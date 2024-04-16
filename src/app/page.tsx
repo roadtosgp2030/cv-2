699fdc6173cab8d6f42571471c938147ab3a8f85
Obar <ducwebdev@gmail.com>
2024-04-16 20:18:07 +0700

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import { Metadata } from "next";
 import { Section } from "@/components/ui/section";
 import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { RESUME_DATA } from "@/data/resume-data";
+import { RESUME_DATA_ENG } from "@/data/resume-data";
 import { ProjectCard } from "@/components/project-card";
 
 export const metadata: Metadata = {
-  title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
-  description: RESUME_DATA.summary,
+  title: `${RESUME_DATA_ENG.name} | ${RESUME_DATA_ENG.about}`,
+  description: RESUME_DATA_ENG.summary,
 };
 
 export default function Page() {
@@ -20,22 +20,22 @@ export default function Page() {
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between">
           <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
+            <h1 className="text-2xl font-bold">{RESUME_DATA_ENG.name}</h1>
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-              {RESUME_DATA.about}
+              {RESUME_DATA_ENG.about}
             </p>
             <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
               <a
                 className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                href={RESUME_DATA.locationLink}
+                href={RESUME_DATA_ENG.locationLink}
                 target="_blank"
               >
                 <GlobeIcon className="size-3" />
-                {RESUME_DATA.location}
+                {RESUME_DATA_ENG.location}
               </a>
             </p>
             <div className="flex flex-col gap-y-1 gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-              {RESUME_DATA.contact.email ? (
+              {RESUME_DATA_ENG.contact.email ? (
                 <div className="flex items-center gap-2">
                 <Button
                   className="size-8"
@@ -43,14 +43,14 @@ export default function Page() {
                   size="icon"
                   asChild
                 >
-                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                  <a href={`mailto:${RESUME_DATA_ENG.contact.email}`}>
                     <MailIcon className="size-4" />
                   </a>
                 </Button>
-                <p>{RESUME_DATA.contact.email}</p>
+                <p>{RESUME_DATA_ENG.contact.email}</p>
                 </div>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
+              {RESUME_DATA_ENG.contact.tel ? (
                 <div className="flex items-center gap-2">
                   <Button
                     className="size-8"
@@ -58,14 +58,14 @@ export default function Page() {
                     size="icon"
                     asChild
                   >
-                    <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                    <a href={`tel:${RESUME_DATA_ENG.contact.tel}`}>
                       <PhoneIcon className="size-4" />
                     </a>
                   </Button>
-                  <p>{RESUME_DATA.contact.tel}</p>
+                  <p>{RESUME_DATA_ENG.contact.tel}</p>
                 </div>
               ) : null}
-              {RESUME_DATA.contact.social.map((social) => (
+              {RESUME_DATA_ENG.contact.social.map((social) => (
                 <div className="flex items-center gap-2" key={social.name}>
                   <Button
                     className="size-8"
@@ -82,35 +82,41 @@ export default function Page() {
               ))}
             </div>
             <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
-              {RESUME_DATA.contact.email ? (
-                <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                  <span className="underline">{RESUME_DATA.contact.email}</span>
+              {RESUME_DATA_ENG.contact.email ? (
+                <a href={`mailto:${RESUME_DATA_ENG.contact.email}`}>
+                  <span className="underline">{RESUME_DATA_ENG.contact.email}</span>
                 </a>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className="underline">{RESUME_DATA.contact.tel}</span>
+              {RESUME_DATA_ENG.contact.tel ? (
+                <a href={`tel:${RESUME_DATA_ENG.contact.tel}`}>
+                  <span className="underline">{RESUME_DATA_ENG.contact.tel}</span>
                 </a>
               ) : null}
             </div>
           </div>
 
           <Avatar className="size-28">
-            <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
-            <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
+            <AvatarImage alt={RESUME_DATA_ENG.name} src={RESUME_DATA_ENG.avatarUrl} />
+            <AvatarFallback>{RESUME_DATA_ENG.initials}</AvatarFallback>
           </Avatar>
         </div>
 
         <Section>
-          <h2 className="text-xl font-bold">Giới thiệu</h2>
+          <h2 className="text-xl font-bold">
+            {/* Giới thiệu */}
+            Introduction
+          </h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            {RESUME_DATA.summary}
+            {RESUME_DATA_ENG.summary}
           </p>
         </Section>
 
         <Section>
-          <h2 className="text-xl font-bold">Kinh nghiệm</h2>
-          {RESUME_DATA.work.map((work) => {
+          <h2 className="text-xl font-bold">
+            {/* Kinh nghiệm */}
+            Experience
+            </h2>
+          {RESUME_DATA_ENG.work.map((work) => {
             return (
               <Card key={work.company}>
                 <CardHeader>
@@ -150,18 +156,24 @@ export default function Page() {
         </Section>
 
         <Section>
-          <h2 className="text-xl font-bold">Công nghệ</h2>
+          <h2 className="text-xl font-bold">
+            {/* Công nghệ */}
+            Technology
+            </h2>
           <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
+            {RESUME_DATA_ENG.skills.map((skill) => {
               return <Badge style={{background: skill.includes('Solid') ? '' : skill.includes('Good') ? '#666' : '#999' }} key={skill}>{skill}</Badge>;
             })}
           </div>
         </Section>
 
         <Section className="print-force-new-page scroll-mb-16">
-          <h2 className="text-xl font-bold">Dự án</h2>
+          <h2 className="text-xl font-bold">
+            {/* Dự án */}
+            Project
+            </h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-            {RESUME_DATA.projects.map((project) => {
+            {RESUME_DATA_ENG.projects.map((project) => {
               return (
                 <ProjectCard
                   key={project.title}
@@ -176,8 +188,11 @@ export default function Page() {
         </Section>
 
         <Section>
-          <h2 className="text-xl font-bold">Học vấn</h2>
-          {RESUME_DATA.education.map((education) => {
+          <h2 className="text-xl font-bold">
+            {/* Học vấn */}
+            Education
+            </h2>
+          {RESUME_DATA_ENG.education.map((education) => {
             return (
               <Card key={education.school}>
                 <CardHeader>
@@ -200,10 +215,10 @@ export default function Page() {
       <CommandMenu
         links={[
           {
-            url: RESUME_DATA.personalWebsiteUrl,
+            url: RESUME_DATA_ENG.personalWebsiteUrl,
             title: "Personal Website",
           },
-          ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
+          ...RESUME_DATA_ENG.contact.social.map((socialMediaLink) => ({
             url: socialMediaLink.url,
             title: socialMediaLink.name,
           })),
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -137,3 +137,122 @@ export const RESUME_DATA = {
     },
   ],
 } as const;
+
+export const RESUME_DATA_ENG = {
+  name: "Vu Ngoc Duc",
+  initials: "BJ",
+  location: "Hamlet 1, Vinh Quynh Village, Thanh Tri, Ha Noi",
+  locationLink: "https://www.google.com/maps/@20.9396192,105.8369638,18.75z?entry=ttu",
+  about:
+    "Frontend Developer",
+  summary:
+    "As a Frontend-oriented programmer, I aspire to create comprehensive interfaces that meet both aesthetic needs and user experience.",
+  avatarUrl: "https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/422959168_336372986053086_2561621154019638612_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4JBpXk25zvUAb4rAZ3c&_nc_ht=scontent.fhan5-8.fna&oh=00_AfAVCv_tp8L-iLHm4TBoROhd9dhb4ea3vHTHab4Gj9xoeQ&oe=662443F7",
+  personalWebsiteUrl: "https://jarocki.me",
+  contact: {
+    email: "ducwebdev@gmail.com",
+    tel: "+84334281675",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/2023newbie",
+        icon: GitHubIcon,
+      },
+      // {
+      //   name: "Hackerrank",
+      //   url: "https://www.hackerrank.com/profile/ducwebdev",
+      //   icon: HackerrankIcon,
+      // },
+    ],
+  },
+  education: [
+    {
+      school: "Funix - A center education of FPT",
+      degree: "Javascript Fullstack Certificate",
+      start: "3/2023",
+      end: "10/2023",
+    },
+    {
+      school: "Hanoi Industries and Economics College",
+      degree: "Business Administration",
+      start: "9/2019",
+      end: "7/2022",
+    },
+  ],
+  work: [
+    {
+      company: 'Vaix Vietnam',
+      link: 'https://vaixgroup.com/',
+      title: 'Intern Frontend',
+      start: '3/2024',
+      end: 'current',
+      description: 'Using plain JavaScript to add functionality to applications on the data management platform for businesses in Japan, Kintone. This includes customizing features with JavaScript, CSS, and working with the publisher\'s API.'
+    }
+  ],
+  skills: [
+    "JavaScript (Solid)",
+    "CSS & TailwindCSS (Good)",
+    "ReactJS (Good)",
+    "TypeScript (Learning)",
+  ],
+  projects: [
+    {
+      title: "Fitness",
+      techStack: [
+        "Side Project",
+        "TypeScript - ReactJS",
+        "Tailwind",
+        "Responsive",
+        "Animation"
+      ],
+      description: "A landing page introducing a gym facility.",
+      link: {
+        href: "https://fitness-p78w.onrender.com/",
+      },
+    },
+    {
+      title: "ProShop",
+      techStack: [
+        "Following Code",
+        "ReactJS",
+        "React-router",
+        "Redux/toolkit"
+      ],
+      description: "A website for purchasing technology gadgets.",
+      link: {
+        href: "https://proshop-x6f6.onrender.com/",
+      }
+    },
+    {
+      title: "BoutiqueShop",
+      techStack: ["Side Project", "ReactJS", "React-router", "Redux", "pure CSS"],
+      description:
+        "An e-commerce website for purchasing Apple products.",
+      logo: JarockiMeLogo,
+      link: {
+        label: "github.com",
+        href: "https://ecommerce-hnh0.onrender.com",
+      },
+    },
+    {
+      title: "Booking",
+      techStack: ["Side Project", "ReactJS", "React-router", "pure CSS"],
+      description:
+        "A hotel search and booking website.",
+      logo: JarockiMeLogo,
+      link: {
+        href: "https://booking-fq7o.onrender.com/",
+      },
+    },
+    {
+      title: "Tailwind Landing Pages",
+      techStack: ["Side Project", "TailwindCSS"],
+      description:
+        "Compilation of landing pages using Tailwind.",
+      logo: JarockiMeLogo,
+      link: {
+        href: "https://2023newbie.github.io/landing-page-tailwind/",
+      },
+    },
+  ],
+} as const;
