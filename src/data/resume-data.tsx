eb4be6ad74677a28ecfda6b586a526a9c4737bcb
Vu Duc <ducvn@vaixgroup.com>
2024-11-20 19:20:45 +0700

    update info

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -22,13 +22,13 @@ import { GitHubIcon, HackerrankIcon } from "@/components/icons";
 export const RESUME_DATA = {
   name: "Vũ Ngọc Đức",
   initials: "BJ",
-  location: "Cụm 1, Vĩnh Quỳnh, Thanh Trì, Hà Nội",
-  locationLink: "https://www.google.com/maps/@20.9396192,105.8369638,18.75z?entry=ttu",
+  location: "Xa La, Phúc La, Hà Đông, Hà Nội",
+  locationLink: "https://www.google.com/maps/place/Wicreset/@20.9620611,105.792678,781m/data=!3m1!1e3!4m14!1m7!3m6!1s0x3134531ffd5e25f3:0x4baa44e913b1ca17!2sMasteri+West+Heights!8m2!3d20.9981814!4d105.7404085!16s%2Fg%2F11lslqq3gs!3m5!1s0x3135ad25e4e8d2ad:0x1af157c184711e85!8m2!3d20.9604855!4d105.7943843!16s%2Fg%2F11c0w3f9fl?entry=ttu&g_ep=EgoyMDI0MTExNy4wIKXMDSoASAFQAw%3D%3D",
   about:
-    "Lập trình viên Frontend - ReactJS",
+    "Lập trình viên Frontend",
   summary:
     "Là một lập trình viên định hướng theo Frontend, mình mong muốn tạo nên các giao diện hoàn chỉnh, đáp ứng nhu cầu thẩm mỹ cũng như trải nghiệm người dùng.",
-  avatarUrl: "https://scontent.fhan15-1.fna.fbcdn.net/v/t39.30808-6/422959168_336372986053086_2561621154019638612_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ivZd8nAjHpkQ7kNvgFhugXd&_nc_zt=23&_nc_ht=scontent.fhan15-1.fna&_nc_gid=ATA7rq7MbQZRUg6IhcClkBw&oh=00_AYCIUVzqd42mEFj6mB9dptQcgxyzKsZHJUm86MLvluV55A&oe=67352A77",
+  avatarUrl: "https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/422959168_336372986053086_2561621154019638612_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=54KwOW1Hx24Q7kNvgGKdusy&_nc_zt=23&_nc_ht=scontent.fhan5-8.fna&_nc_gid=AFrXWvBSrapyYhm61MujFqC&oh=00_AYDU1r0J4CNSUJxPAaEmBfDm-b1wKFfZW3bCe05zD9HdaQ&oe=6743AAF7",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
     email: "ducwebdev@gmail.com",
@@ -50,7 +50,7 @@ export const RESUME_DATA = {
     {
       school: "Funix - Trung tâm đào tạo lập trình",
       degree: "Chứng chỉ lập trình viên Fullstack",
-      start: "3/2023",
+      start: "12/2022",
       end: "10/2023",
     },
     {
@@ -65,20 +65,21 @@ export const RESUME_DATA = {
       company: 'Vaix Vietnam',
       link: 'https://vaixgroup.com/',
       title: 'Frontend',
-      start: '12/2023',
+      start: '10/2023',
       end: 'nay',
       description: 'Sử dụng ReactJS cùng các thư viện liên quan xây dựng và phát triển các dự án theo yêu cầu của công ty. Viết plug-in cho khách hàng trên nền tảng quản lý dữ liệu doanh nghiệp Kintone của Nhật Bản.'
     }
   ],
   skills: [
-    "JavaScript",
-    "CSS+Tailwind",
-    "ReactJS+Redux",
-    "TypeScript",
+    "JavaScript (mạnh)",
+    "CSS (mạnh)",
+    "ReactJS (mạnh)",
+    "NextJS",
+    "VueJS",
   ],
   projects: [
     {
-      title: "Fitness",
+      title: "Fitness (click truy cập)",
       techStack: [
         "Side Project",
         "TypeScript - ReactJS",
@@ -92,7 +93,7 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "ProShop",
+      title: "ProShop (click truy cập)",
       techStack: [
         "Following Code",
         "ReactJS",
@@ -115,26 +116,26 @@ export const RESUME_DATA = {
     //     href: "https://ecommerce-hnh0.onrender.com",
     //   },
     // },
-    {
-      title: "Booking",
-      techStack: ["Side Project", "ReactJS", "React-router", "pure CSS"],
-      description:
-        "Một trang web tìm kiếm khách sạn và đặt phòng.",
-      logo: JarockiMeLogo,
-      link: {
-        href: "https://booking-fq7o.onrender.com/",
-      },
-    },
-    {
-      title: "Tailwind Landing Pages",
-      techStack: ["Side Project", "TailwindCSS"],
-      description:
-        "Tổng hợp các landing page bằng Tailwind.",
-      logo: JarockiMeLogo,
-      link: {
-        href: "https://roadtosgp2030.github.io/landing-page-tailwind/",
-      },
-    },
+    // {
+    //   title: "Booking",
+    //   techStack: ["Side Project", "ReactJS", "React-router", "pure CSS"],
+    //   description:
+    //     "Một trang web tìm kiếm khách sạn và đặt phòng.",
+    //   logo: JarockiMeLogo,
+    //   link: {
+    //     href: "https://booking-fq7o.onrender.com/",
+    //   },
+    // },
+    // {
+    //   title: "Tailwind Landing Pages",
+    //   techStack: ["Side Project", "TailwindCSS"],
+    //   description:
+    //     "Tổng hợp các landing page bằng Tailwind.",
+    //   logo: JarockiMeLogo,
+    //   link: {
+    //     href: "https://roadtosgp2030.github.io/landing-page-tailwind/",
+    //   },
+    // },
   ],
 } as const;
 
